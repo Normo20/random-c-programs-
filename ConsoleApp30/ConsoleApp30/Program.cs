e5415d8a649acd8a4161e6160385e68d23e322b7
James Norman <55443042+Normo20@users.noreply.github.com>
2021-12-03 18:07:32 +1000

    Add files via upload

---
+++ ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Permutations
+{
+    // Helper method for outputting an array.
+    private static void PrintArray(string[] array)
+    {
+        foreach (string element in array)
+        {
+            Console.Write($"{element} ");
+        }
+        Console.WriteLine();
+    }
+    private static void Generate(int k, string[] A)
+    {
+        if (k == 1)
+        {
+            PrintArray(A);
+        }
+        else
+        {
+            k = A.Length;
+            Generate(k - 1, A);
+
+            for (int i = 0; i < k - 1; i++)
+            {
+                if (k % 2 == 0)
+                {
+                    string temp = A[i];
+                    A[i] = A[k - 1];
+                    A[k - 1] = temp;
+                }
+                else
+                {
+                    string temp = A[0];
+                    A[0] = A[k - 1];
+                    A[k - 1] = temp;
+                }
+                Generate(k - 1, A);
+            }
+        }
+    }
+
+    // Helper method for invoking Generate.
+    private static void Generate(string[] array)
+    {
+        Generate(array.Length, array);
+    }
+
+    public static void Main(string[] args)
+    {
+        Generate(2, args);
+    }
+}
---
+++ ConsoleApp32/ConsoleApp32/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class ExceptionTest
+    {
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new System.DivideByZeroException();
+            return x / y;
+        }
+        static void Main()
+        {
+            // Input for test purposes. Change the values to see
+            // exception handling behavior.
+            double a = 98, b = 1;
+            double result = 0;
+
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Attempted divide by zero.");
+            }
+        }
+    }
+}
