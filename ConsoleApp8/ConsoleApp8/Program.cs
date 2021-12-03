b80b97e8bc7be88f1a2b554d14e0ab8da9c9d42d
James Norman <55443042+Normo20@users.noreply.github.com>
2021-12-03 17:51:50 +1000

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salesCode = "";
+            double discount = 0.0;
+            Console.Write("Enter Sales Code: ");
+            salesCode = Console.ReadLine();
+            if ((salesCode != "A") || (salesCode != "B"))
+            {
+                discount = 0.10;
+            }
+            else
+            {
+                discount = 0;
+            }
+            Console.WriteLine("\n Discount applied is {0:F2}\n", discount);
+
+            Console.WriteLine("Press any key to exit:");
+            Console.ReadKey();
+
+        }
+    }
+}
