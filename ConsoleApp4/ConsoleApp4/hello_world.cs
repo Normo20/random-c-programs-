a0d872bf6cb1e0c472dba209e8c59a5577994ea5
James Norman <55443042+Normo20@users.noreply.github.com>
2021-12-03 17:49:05 +1000

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/hello_world.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main()
+    {
+        // Replace the following line with code to display "Hello World - Welcome to CAB201" on a line by itself
+        // ...
+        
+        Console.WriteLine("Hello World - Welcome to CAB201");
+
+
+        // Replace the following line with code to wait for the user to press enter
+        // ...
+        Console.ReadLine();
+    }
+}
