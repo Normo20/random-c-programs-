5309de79178d1d38d702c1985bb7648672a94465
James Norman <55443042+Normo20@users.noreply.github.com>
2021-12-03 17:58:06 +1000

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RandomArray
+{
+    public class RandomArrayNoDuplicates
+    {
+        static Random rng = new Random();
+        
+        /// <summary>
+        /// Creates an array with each element a unique integer
+        /// between 1 and 45 inclusively.
+        /// </summary>
+        /// <param name="size"> length of the returned array < 45
+        /// </param>
+        /// <returns>an array of length "size" and each element is
+        /// a unique integer between 1 and 45 inclusive </returns>
+        public static int[] ArrayWithNoDuplicates(int size)
+        {
+           
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                int randNum = rng.Next(1, 45);
+                array [i] = randNum;
+                Console.WriteLine("{0}", array[i]);
+                
+
+            }
+
+
+            return array;
+        }
+
+      
+    }
+
+
+
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linear_Search
+{
+    class Program
+    {
+
+        //main
+        static void Main(string[] args)
+        {
+
+            const string Start_Message = "\n\tWelcome to Linear Search Demonstration\n"
+                                        + "\n The array contains the following 9 words:\n\n";
+
+            string[] words = {"the", "quick", "brown", "fox",
+                              "jumps", "over", "the", "lazy", "dog"};
+
+ 
+            OutputMessage(Start_Message);
+            DisplayArray(words);
+
+         
+
+        } //end Main
+
+       
+
+
+        
+        /// Displays the elements of the array "words"
+        /// </summary>
+        /// <param name="words">array to be displayed</param>
+        static void DisplayArray(string[] words)
+        {
+
+            foreach (string element in words)
+            {
+                OutputMessage("\t" + element + "\n");
+            }
+            Console.WriteLine();
+
+        }//end DisplayArray
+
+        /// <summary>
+        /// Displays the outcome of the linear search
+        /// </summary>
+        /// <param name="word"></param>
+     
+
+        /// <summary>
+        /// Outputs the string "s"
+        /// </summary>
+        /// <param name="s">String to be output</param>
+        static void OutputMessage(string s)
+        {
+
+            Console.Write(s);
+        }// end OutPutMessage
+
+       
+
+    }//end class
+}//end namespace
